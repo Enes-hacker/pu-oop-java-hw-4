a3eb4fa62d75f3570309ffc41e70449fab35dde0
Enes-hacker <63426211+Enes-hacker@users.noreply.github.com>
2021-02-11 00:43:17 +0100

    Add files via upload

---
+++ src/Game/Gameboard.java
@@ -0,0 +1,54 @@
+package Game;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Gameboard {
+    public class Board extends JFrame implements MouseListener {
+        public static final int TILE_SIDE_COUNT = 6;
+
+
+        public void mouseClicked(MouseEvent e) {
+
+
+
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+        @Override
+
+
+        public void paint(Graphics g) {
+
+            // super.paint(g);
+
+            for(int row = 0 ;  row  < 8; row++){
+
+                for(int col = 0; col < 8;col++){
+
+
+
+}
+                }
+
+            }
+        }
+    }
